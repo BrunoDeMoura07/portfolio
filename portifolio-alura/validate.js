b4e8c141233cc4de1c12063880da12a0203e248a
Bruno de Moura <brunnofarias16@gmail.com>
2023-07-22 00:06:09 -0300

    treinando javascript

--- portifolio-alura/validate.js
+++ portifolio-alura/validate.js
@@ -1,1 +1,22 @@
-//Seu JavaScript de validação aqui
+// await == aguarde.
+// then == então. 
+// catch == pegue.
+// finally == finalmente.
+// try == tentar
+// curso 7 alura: melhorendo o mesmo codigo.
+async function buscaEndereco(cep){
+    try{ 
+      const consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+      const consultaCepConvertida = await consultaCep.json();
+      if (consultaCepConvertida.erro){
+        throw Error('CEP inexistente!');
+      }
+      console.log(consultaCepConvertida);
+      return consultaCepConvertida;
+      } catch(erro) {
+          console.log(erro);
+          }
+}
+
+let cep = document.getElementById('cep');
+cep.addEventListener("focusout",() => buscaEndereco(cep.value));
