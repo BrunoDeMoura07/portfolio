752334d5bb8f2643d9c50a79658f73759a20a706
Bruno de Moura <brunnofarias16@gmail.com>
2023-10-15 17:03:53 -0300

    testando alterações para um novo layout

---
+++ portifolio-alura/assets/script/animacao-titulo.js
@@ -0,0 +1,23 @@
+const frontEndText = document.getElementById('titulo-bio');
+
+    function getRandomColor() {
+      const letters = '0123456789ABCDEF';
+      let color = '#';
+      for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    }
+
+    function changeColor() {
+      const randomColor = getRandomColor();
+      frontEndText.style.color = randomColor;
+    }
+
+    frontEndText.addEventListener('mouseover', () => {
+      const intervalId = setInterval(changeColor, 100);
+      frontEndText.addEventListener('mouseout', () => {
+        clearInterval(intervalId);
+        frontEndText.style.color = 'var(--cor-azul-padrao)';
+      });
+    });
