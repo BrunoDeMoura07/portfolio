3b6a7b66ebdd720e86dc1d92d1016fe2916a26ce
Bruno de Moura <Brunnofarias16@gmail.com>
2024-02-04 10:54:01 -0300

    conserto no js para adaptar a funcionalidade de menu responsivo

--- assets/script/bnt-menu.js
+++ assets/script/bnt-menu.js
@@ -1,19 +1,22 @@
-openMenu.addeventListener("click", () => {
+const openMenu = document.getElementById("openMenu");
+const closeMenu = document.getElementById("closeMenu");
+const menu = document.getElementById("menu");
+
+openMenu.addEventListener("click", () => {
   menu.style.display = "flex";
-  menu.style.right = menu.offsetwidht + -1 + "px";
-  openMenu.style.display = 'none'
+  menu.style.right = -1 * menu.offsetWidth + "px";
+  openMenu.style.display = "none";
 
   setTimeout(() => {
-    menu.style.display = "flex";
     menu.style.right = "0";
   }, 10);
 });
 
-closeMenu.addeventListener("click", () => {
+closeMenu.addEventListener("click", () => {
   menu.style.opacity = "0";
-  menu.style.right = menu.offsetwidht + -1 + "px";
-  openMenu.removeAttribute('style')
-  
+  menu.style.right = -1 * menu.offsetWidth + "px";
+  openMenu.style.display = "block";
+
   setTimeout(() => {
     menu.removeAttribute("style");
   }, 200);
